556a3e57ea24b0a47b29e1e01f5ba4d7be2db9d4
palakmhjn <68996509+palakmhjn@users.noreply.github.com>
2020-08-06 17:42:19 +0530

    New File

---
+++ If_Else_Statements.py
@@ -0,0 +1,39 @@
+# Finding Even Odd numbers
+# x= float(input("enter a number"))
+# if x % 2 == 0:
+#     print("Even number")
+# else:
+#     print("odd number")
+# Prime Number
+# x= int(input("Enter a number"))
+# if x>0:
+#     for i in range(2,x):
+#         if x % 2 ==0:
+#             print("not a prime number")
+#             break
+#         else:
+#             print("prime number")
+#             break
+# else:
+#     print("give a positive number")
+
+# Nested If
+# x= int(input("Enter a number"))
+# if x>0:
+#     if x<5 and x <10:
+#         print("less than 5")
+#     else:
+#         print("greater than 5")
+# elif x >= 10:
+#     print("greater than equal to 10")
+# else :
+#     print("enter a number again")
+
+a,b= 200,33
+if b>a:
+    print("b is greater than a")
+elif a==b:
+    print("a and b are equal")
+else:
+    print("a is greater than b")
+
