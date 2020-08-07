1cbfea39ef1a85a31367594d454b8b84e45499c2
palakmhjn <68996509+palakmhjn@users.noreply.github.com>
2020-08-07 13:36:08 +0530

    Python files

---
+++ Palak/New_Test.py
@@ -0,0 +1,25 @@
+#print('Hello')
+# a=7
+# print(a)
+# myfloat=float(7)
+# print(myfloat)
+# mystring= "Don't Worry"
+# print(mystring)
+# a=1
+# b=2
+# c="Hello"
+# print(a + b + c)
+# x="awesome"
+# def myfunc():
+#     print("python is"+x)
+# myfunc()
+#Global Variables
+# x="web application"
+# def myfunc():
+#     x="programming"
+#     print("python" +" " + x)
+# myfunc()
+# print("python used for" +" " + x)
+
+b= list(("palak","Amit","Akash","Deepak"))
+print(b)
